cea8a55f81b03dd781b9c77d00cb415d841fcbf3
Lillian26 <lilliannamusisi@gmail.com>
2022-03-06 02:33:00 +0300

    Updated mobile design, Started minitin fn.

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className='mx-40 mt-20 mb-5'>
+      <h1 className='text-[25px] font-bold'>About</h1>
     </div>
   )
 }
