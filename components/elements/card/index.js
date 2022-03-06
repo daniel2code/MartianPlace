51a6e86c6c670e2bcd53a4a6c1bd734ad924c810
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2022-03-06 07:11:07 -0800

    correction of the card components

--- components/elements/card/index.js
+++ components/elements/card/index.js
@@ -2,27 +2,38 @@ import Image from 'next/image';
 import React from 'react';
 import { character } from '../../../assets/images';
 
-const Card = () => {
+const Card = ({ img, charName, description, moreDetail }) => {
   return (
     <section className="border border-light-grey-1 bg-line-light md:p-6 p-2 rounded-xl mt-8">
       <div>
         <div>
           <h3 className="font-light mb-3">Game</h3>
         </div>
-        <div>
-          <Image src={character} />
+        <div className="w-full">
+          <Image src={img || character} height={500} width={500} />
         </div>
-        <div className="mt-3 mb-3">
-          <h2 className="font-bold text-pink">Cyberpunkez</h2>
-        </div>
-        <div className="mb-3">
-          <p className="font-light">
-            In Night City, a mercenary known as V navigates a dystopian society.
-          </p>
-        </div>
-        <div>
-          <p className="font-light text-light-grey-1">9 available NFT’s</p>
-        </div>
+        {charName && (
+          <div className="mt-3 mb-3">
+            <h2 className="font-bold text-pink">{charName || 'Cyberpunkez'}</h2>
+          </div>
+        )}
+
+        {description && (
+          <div className="mb-3">
+            <p className="font-light">
+              {description ||
+                'In Night City, a mercenary known as V navigates a dystopian'}
+              society.
+            </p>
+          </div>
+        )}
+        {moreDetail && (
+          <div>
+            <p className="font-light text-light-grey-1">
+              {moreDetail && '9 available NFT’s'}
+            </p>
+          </div>
+        )}
       </div>
     </section>
   );
